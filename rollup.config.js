0ddde0345ef7eac60c5b7814d5aab4520394b330
Dmitriy Karpov <dmtkpv@gmail.com>
2022-12-16 23:57:24 +0200

    Release 0.0.9

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,14 @@
-import terser from '@rollup/plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/index.mjs',
+        file: 'dist/index.js',
         banner: '#!/usr/bin/env node',
     },
     plugins: [
         nodeResolve(), // imports from node_modules
         commonjs(), // converts cjs modules to es6
-        terser() // minfiy
     ]
 };
