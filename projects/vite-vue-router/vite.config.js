7259c3f8bfde887e2dc5fe1a7dbb09744d2a3ad5
Dmitriy Karpov <dmtkpv@gmail.com>
2023-01-27 20:47:53 +0200

    Release 0.0.16

--- projects/vite-vue-router/vite.config.js
+++ projects/vite-vue-router/vite.config.js
@@ -1,6 +1,13 @@
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 export default {
+
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
 
     server: {
         port: 49003
